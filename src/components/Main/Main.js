32ba13f469a423b6498402686aa679407e9a1198
dnrgus1127 <dnrgus1127@naver.com>
2022-08-20 21:01:25 +0900

    FEAT : Contents Add commit

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -19,8 +19,12 @@ const MainContents = styled.div`
     width: 80%;
     max-width: 1000px;
     margin: 0 auto;
-    display: flex;
 
+`
+
+const SubContainer = styled.div`
+    width: 100%;
+    display: flex;
 `
 
 const LeftContainer = styled.div`
@@ -41,6 +45,9 @@ const A = styled.a`
     font-size: 1rem;
 
 `
+const TextAtag = styled.a`
+    color: black;
+`
 
 const H1 = styled.h1`
     font-family: 'Noe Display-medium';
@@ -55,26 +62,58 @@ const P = styled.p`
     font-family: 'OpenSans_VariableFont';
 
 `
-const PNoe =styled.p`
+const PNoe = styled.p`
     font-family: 'Noe Display-medium';
     font-size: 2.5rem;
 `
 
+
+
 const PText = styled.p`
-    width: 80%;
     font-family: 'OpenSans_VariableFont';
     line-height: 1.7em;
+    font-size: ${({fontSize})=> {return fontSize;}};
     font-weight: 300;
+    text-align: ${({float}) =>{
+
+        if(float === "right")
+        {
+            return float;
+        }
+        else if(float ==='center')
+        {
+            return float;
+        }
+    }
+    };
 `
-// const SizedBox = styled.div`
-//     width: 8.3333%;
-//     float: left;
-//     /* height: 10px; */
-// `
+
+const PTextW = styled(PText)`
+    width: 80%;
+
+`
+const SizedBox = styled.div`
+    width: 1500px;
+    /* background-color: red; */
+    height: 10rem;
+    /* height: 10px; */
+    display: block;
+`
 
 const Img = styled.img`
     width: 100%;
 
+`
+
+const CenterContainer = styled.div`
+    width: 100%;
+    text-align: center;
+`
+const PaddingContainer = styled.div`
+    padding: ${({padding})=>{
+        return padding;
+    }} ;
+    width: 100%;
 `
 
 
@@ -89,18 +128,36 @@ const Main = () => {
                 <H1>Simple And Serene Squarespace Website For Wellness Brand</H1>
             </HeaderContainer>
             <MainContents>
-                <LeftContainer>
-                    <P>ABOUT THE CLIENT</P>
-                    <PNoe>eNourish, Health & Wellness Brand</PNoe>
-                    <PText>Isa Griffin is a personal and professional coach based in California and the founder of eNourish, a health and wellness brand that offers habit-based coaching, shop recommendations, and a soon-coming resource platform. Being a model, dancer, and beauty professional, health and wellness have always been a big passion for Isa. Now she gets to use that passion to help others develop and enjoy a strong connection with themselves. 
+                <SubContainer>
+                    <LeftContainer>
+                        <P>ABOUT THE CLIENT</P>
+                        <PNoe>eNourish, Health & Wellness Brand</PNoe>
+                        <PTextW>Isa Griffin is a personal and professional coach based in California and the founder of eNourish, a health and wellness brand that offers habit-based coaching, shop recommendations, and a soon-coming resource platform. Being a model, dancer, and beauty professional, health and wellness have always been a big passion for Isa. Now she gets to use that passion to help others develop and enjoy a strong connection with themselves.
 
-</PText>
-                </LeftContainer>
-                <RightContainer>
-                    <Img src={Image}></Img>
-                </RightContainer>
+                        </PTextW>
+                    </LeftContainer>
+                    <RightContainer>
+                        <Img src={Image}></Img>
+                    </RightContainer>
+                </SubContainer>
+                <SubContainer>
+                    <SizedBox></SizedBox>
+                </SubContainer>
+                <SubContainer>
+                    <CenterContainer>
+                        <P>ABOUT THE DESIGN</P>
+                        <PText>For this project, our team worked on a <TextAtag href='/'>Restyle</TextAtag> of our <TextAtag href='/'>Nite Template for Squarespace</TextAtag>. One of the main goals was to showcase the clean, natural and down-to-earth feel of the brand. We accomplished that through the color palette and photography. We also added small graphic touches, such as the waterbird icon, and arches to add to the overall vibe of the brand. Another fun aspect of this project, was transforming our Nite template from its original dark and moody vibe to the exact opposite. We also made the site more dynamic and added movement with parallax.</PText>
+                    </CenterContainer>
+                </SubContainer>
+                <SubContainer>
+                    <PaddingContainer padding={"1rem"}>
+                        <PText fontSize="1.5rem">“I really enjoyed the overall strategy that GoLive has developed to really understand your individual brand. They were able to meet me where I was. They took everything that I had, and turned it into everything that I wanted.”</PText>
+                        <PText float="right">— Isa Griffin</PText>
+                    </PaddingContainer>
+                </SubContainer>
 
             </MainContents>
+
         </Container>
     )
 }
